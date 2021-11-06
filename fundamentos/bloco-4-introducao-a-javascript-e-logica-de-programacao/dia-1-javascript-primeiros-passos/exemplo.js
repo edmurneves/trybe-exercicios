62987c449b35f9025d3f13cf7c479e34bead4a72
Edmur Neves <edmurneves@gmail.com>
2021-11-06 17:22:34 -0300

    comite inicial

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exemplo.js
@@ -0,0 +1,99 @@
+/* Exercicio 1
+
+const a = 10;
+const b = 5;
+
+soma = (a + b);
+subtracao = (a - b);
+multiplicacao = (a * b);
+divisao = (a / b);
+modulo = (a % b);
+
+console.log( soma, subtracao, multiplicacao, divisao, modulo);
+ */
+
+/* Exercicio 2
+
+const a = 3;
+const b = 5;
+
+if (a > b){
+    maior = a;
+}else {
+    maior = b;
+}
+console.log(maior); */
+
+/* Exercício 3
+
+const a = 5
+const b = 7
+const c = 10
+
+if (a > b && a > c){
+    maior = a;
+}else if (b > a && b > c){
+    maior = b
+}else {
+    maior = c;
+}
+console.log(maior); */
+
+/* Exercicio 4
+const valor = -2;
+
+if (valor == 0){
+    ret = "zero"
+}else if (valor > 0){
+    ret = "Positivo"
+}else {
+    ret = "Negativo"
+}
+console.log('O valor é =', ret); */
+
+/* Exercício 5
+const a = 20;
+const b = 30;
+const c = 120;
+let soma;
+
+soma = a + b + c;
+
+if (soma == 180){
+    ret = true;
+    console.log("É um triangulo.")
+}else {
+    ret = false;
+    console.log("Não é um triangulo.")
+} */
+
+/* Exercicio 6
+const xadrez = "REI";
+
+peca = xadrez.toLowerCase();
+
+if (peca == "bispo"){
+    console.log("movimento em diagonal")
+} else if ( peca == "cavalo"){
+    console.log("Movimento em L");
+} else if (peca == "rei"){
+    console.log("Movimento na horizontal e vertical")
+}else {
+    console.log("Não é uma peça válida")
+} */
+
+const nota = 69;
+
+if (nota < 50){
+    console.log("Nota F")
+} else if (nota >= 50 && nota < 60){
+    console.log("Nota E")
+} else if (nota > 60){
+    console.log("Nota D")
+} else if (nota > 70){
+    console.log("Nota C")
+} else if (nota > 80){
+    console.log("Nota B")
+} else {
+    console.log("Nota Invalida")
+}
