b0431ea31672502792e752afd57bed387c163413
Edmur Neves <edmurneves@gmail.com>
2021-11-06 20:44:02 -0300

    exercicio 11

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exemplo.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exemplo.js
@@ -127,3 +127,52 @@ if (n1 % 2 == 1 || n2 % 2 == 1 || n3 % 2 == 1){
     console.log('false')
 } */
 
+/*Exercicio 10 
+const custoProduto = 100;
+const valorVenda = 180;
+
+let qtdVenda = 1000;
+let valorCustoTotal;
+let lucro;
+
+if ( custoProduto < 0 || valorVenda < 0) {
+    console.log('Valores inválidos');
+}else {    
+    valorCustoTotal = custoProduto * 1.2;
+    lucro = (valorVenda - valorCustoTotal) * qtdVenda;
+    console.log('O lucro da venda será de: ', lucro);
+} */
+
+
+const salarioBruto = 1100;
+
+let aliquotaINSS;
+let aliquotaIR;
+let salarioBase;
+let salarioLiquido;
+
+if (salarioBruto <= 1556.94){
+    aliquotaINSS = salarioBruto * (8/100);
+}else if (salarioBruto <= 2594.92){
+    aliquotaINSS = salarioBruto * (9/100);
+}else if ( salarioBruto <= 5189.82){
+    aliquotaINSS = salarioBruto * (11/100);
+}else {
+    aliquotaINSS = 570.88;
+}
+salarioBase = salarioBruto - aliquotaINSS;
+
+if (salarioBase <= 1903.98){
+    aliquotaIR = 0;
+}else if (salarioBase <= 2826.65){
+    aliquotaIR = (salarioBase * 7.5/100) - 142.80;
+}else if (salarioBase <= 3751.05){
+    aliquotaIR = (salarioBase * 15/100) - 354.80;
+}else if (salarioBase <= 4664.68){
+    aliquotaIR = (salarioBase * 22.5/100) - 636.13;
+}else {
+    aliquotaIR = (salarioBase * 27.5/100) - 869.36;
+}
+
+salarioLiquido = salarioBase - aliquotaIR;
+console.log(`O salário liquido será de: ${salarioLiquido}`);
