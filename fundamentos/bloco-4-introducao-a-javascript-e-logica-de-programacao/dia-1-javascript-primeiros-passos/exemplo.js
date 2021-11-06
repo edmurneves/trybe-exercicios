bba72fb3c43d1bc5831eea09b52b5e8f0ca3a232
Edmur Neves <edmurneves@gmail.com>
2021-11-06 18:50:00 -0300

    exercicios 8 e 0

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exemplo.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exemplo.js
@@ -103,3 +103,27 @@ if (nota < 0){
 } else if (nota > 100){
     console.log("Nota inválida")
 } */
+
+/* Exercicio 8
+
+const n1 = 11;
+const n2 = 15;
+const n3 = 18;
+
+if (n1 % 2 == 0 || n2 % 2 == 0 || n3 % 2 == 0){
+    console.log('true')
+}else {
+    console.log('false')
+} */
+
+/* Exercicio 9
+const n1 = 16;
+const n2 = 12;
+const n3 = 18;
+
+if (n1 % 2 == 1 || n2 % 2 == 1 || n3 % 2 == 1){
+    console.log('true')
+}else {
+    console.log('false')
+} */
+
