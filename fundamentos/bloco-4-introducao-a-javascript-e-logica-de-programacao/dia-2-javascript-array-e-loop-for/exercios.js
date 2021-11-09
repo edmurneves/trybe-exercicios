86ca51f6599699acdcf8394c603a72f9ac28dfba
Edmur Neves <edmurneves@gmail.com>
2021-11-09 12:37:49 -0300

    exercicio 5 ao 8

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercios.js
@@ -30,14 +30,62 @@ if(media > 20){
     console.log('valor menor que 20');
 } */
 
+
+/* 5. Utilizando for , descubra qual o maior valor contido no array e imprima-o; */
 /* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior = 0;
-let aux =0;
+let maior = numbers[0];
+
+
+for(i = 0; i < numbers.length; i++){    
+    if(numbers[i] > maior){
+        maior = numbers[i]
+        }      
+}
+console.log(maior); */
+
+
+/* 5. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado"; */
+
+/* let numbers = [2, 4, 6, 16, 70, 8, 100, 2, 34, 22];
+let impares = [];
+let isNull = false;
 
 for(i = 0; i < numbers.length; i++){
-    aux = numbers[i];
-    if(aux >= maior){
-        maior = aux;
+    if (numbers[i] % 2 !== 0 ){
+        impares.push(numbers[i]);
+    }
+}
+if (impares.length == 0){
+    console.log('Nenhum valor impar encontrado')
+}else {
+    console.log('Existem ',impares.length, ' numeros ímpares.');
+}
+ */
+
+/* 7. Utilizando for , descubra qual o menor valor contido no array e imprima-o; */
+
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menor = numbers[0];
+
+for(let i = 1; i < numbers.length; i++){
+    
+    if(numbers[i] < menor){
+        menor = numbers[i];
     }      
 }
-console.log(maior);  */
+console.log(menor); */
+
+/* 8. Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado; */
+
+/* let array = [];
+
+for(let i = 1; i <=25; i += 1){
+    array.push(i);
+}
+console.log(array);
+
+
+for(let u = 0; u < array.length; u += 1){
+    array[u] = array[u] / 2;
+}
+console.log(array) */
